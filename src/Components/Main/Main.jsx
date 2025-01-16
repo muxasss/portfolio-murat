18eeb8015075213a2f6927ad7739e95984934a1c
Mukhammed <muxalllsss@gmail.com>
2025-01-16 23:21:43 +0600

    add icon

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { RxHamburgerMenu } from 'react-icons/rx'; 
+import { GiHamburgerMenu } from "react-icons/gi";
 import { IoSearch, IoLogoInstagram, IoLogoFacebook, IoLogoWhatsapp, IoMailOutline } from 'react-icons/io5';
 import './Main.css';
 
@@ -14,7 +14,7 @@ const Main = () => {
     <div>
       {/* Иконка гамбургера, которая будет видна только на мобильных устройствах */}
       <div className="hamburger-menu" onClick={toggleMenu}>
-        <RxHamburgerMenu className="Hamburger-icon" size={30} color="#fff" />
+      <GiHamburgerMenu className="Hamburger-icon" size={30} color="#fff" />
       </div>
       <img className='logo' src='logo-ma.png' />
       {/* Иконка поиска, видна только на десктопах */}
